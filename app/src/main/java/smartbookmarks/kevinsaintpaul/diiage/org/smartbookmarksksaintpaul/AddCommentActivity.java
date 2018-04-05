01b8f9ee0bfbe313686e02dc4d713b847841bb7d
Kevin SAINT PAUL <kevin.saintpaul@diiage.org>
2018-04-05 16:39:37 +0200

    Create Layout

---
+++ app/src/main/java/smartbookmarks/kevinsaintpaul/diiage/org/smartbookmarksksaintpaul/AddCommentActivity.java
@@ -0,0 +1,13 @@
+package smartbookmarks.kevinsaintpaul.diiage.org.smartbookmarksksaintpaul;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddCommentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_comment);
+    }
+}
